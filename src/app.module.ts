ad0251873fea1b3d2892ce4d6b918f3edbf15108
willy <willybalt.2017@gmail.com>
2025-07-15 10:27:04 -0400

    se agrego conexion con mongo y se creo el modulo estudents

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { EstudentsModule } from './estudents/estudents.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://admin:12345@cluster0.hewbekg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
+    EstudentsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
