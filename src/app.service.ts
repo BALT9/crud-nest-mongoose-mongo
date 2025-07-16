8cfe46a78e6f4a2270690ab121fae890deff0e2f
willy <willybalt.2017@gmail.com>
2025-07-16 10:38:35 -0400

    se realizo la conexion con la base de datos mongo

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
 
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  constructor(@InjectConnection() private connection: Connection){}
+
+  async onModuleInit(){
+    // condicional que devuelve true o false 
+    const isConnected = this.connection.readyState === 1;
+    console.log(`Estado de conexion: ${isConnected ? "conectado con mongo" : "error al conectar con mongo"}`)
   }
 }
--- src/estudents/estudents.module.ts
+++ src/estudents/estudents.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { EstudentsService } from './estudents.service';
 import { EstudentsController } from './estudents.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Estudent, StudentSchema } from './schemas/estudent.schema';
 
 @Module({
+  // importa el esquema estudent especifico para el modulo estudents
+  imports: [MongooseModule.forFeature([{name: Estudent.name, schema: StudentSchema}])],
   controllers: [EstudentsController],
   providers: [EstudentsService],
 })
